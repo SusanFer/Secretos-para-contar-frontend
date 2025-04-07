f70eaa5a2cdf83c93efbc59e6c95870b8cc7bc32
SusanFer <susanfer2412@gmail.com>
2025-04-06 21:13:43 -0500

    solucionando confllicto de rutas

---
+++ app/routes/libros/route.tsx
@@ -0,0 +1,9 @@
+import BookList from "../home/BookList";
+
+export default function LibrosRoute() {
+  return (
+    <div className="container mx-auto py-8">
+      <BookList />
+    </div>
+  );
+}
